3db3c8a4e24b05ff10e6a27a35fb1ea7c53a866b
Girish Gabra <girish.gabra10@gmail.com>
2017-05-13 12:25:25 -0400

    Api for word suggestions

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ from flask_restful import Api, Resource
 from resources.allresources import Welcome
 from resources.allresources import RandomWord
 from resources.allresources import RhymeForWord
+from resources.allresources import SuggestWord
 
 # Create a flask instance
 app = Flask(__name__)
@@ -10,8 +11,10 @@ app = Flask(__name__)
 api = Api(app)	#Create Api for app instance
 
 # Add the resources to our Api instance
+api.add_resource(Welcome, '/')
 api.add_resource(RandomWord,'/random')
 api.add_resource(RhymeForWord,'/rhyme')
-api.add_resource(Welcome, '/')
+api.add_resource(SuggestWord,'/suggest')
+
 
 
--- resources/allresources.py
+++ resources/allresources.py
@@ -1,7 +1,9 @@
 from flask import request
 from flask_restful import Resource
 from random import randint
+import random
 import pronouncing
+import enchant
 
 # this can be considered as a home page having welcome message
 class Welcome(Resource):
@@ -30,3 +32,15 @@ class RhymeForWord(Resource):
 		rhymeList = pronouncing.rhymes(word)	# Fetch list of rhyming words with the input word
 		rhymeList = ",".join(rhymeList)			# form a string of all rhyming words separated by ','
 		return {'rhymes':rhymeList}				# return json
+
+#this resource will suggest list of 10 words that are close to the input word
+class SuggestWord(Resource):
+	def get(self):
+		args=request.args
+		d=enchant.Dict("en_US")
+		word = args['word']
+		suggestList = list(d.suggest(word))[:10]
+		suggestList = ",".join(suggestList)
+		return {'suggestions': suggestList}
+		
+		
